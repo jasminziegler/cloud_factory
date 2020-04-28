6b3abdc5fb46e56d2a55ee0373e220c6f535f9c4
Jasmin Dschessica <juicy@jessica.web>
2020-04-28 18:56:10 +0200

    added rectangle publisher yey!

--- src/pubRectangBox.cpp
+++ src/pubRectangBox.cpp
@@ -30,7 +30,6 @@ int main(int argc, char** argv)
 
     unsigned int raysAzim         = round(static_cast<unsigned>(360.0 / azimRes));
     unsigned int raysAzimQuadrant = raysAzim / 4;
-    unsigned int counterAzim      = 0;
     unsigned int idx              = 0;
 
     cloud.width    = raysAzim;
@@ -39,44 +38,89 @@ int main(int argc, char** argv)
     cloud.points.resize(cloud.width * cloud.height);
     cloud.header.frame_id = "map";
 
-    std::cout << "rayAzim = " << raysAzim << std::endl;
+    // std::cout << "raysAzim = " << raysAzim << std::endl;
+    // std::cout << "raysAzimQuadrant = " << raysAzimQuadrant << std::endl;
+
+    double       lastX     = 0.0;
+    unsigned int counter1B = 0;
+    double       lastY     = 0.0;
+    double       lastX2    = 0.0;
+    double       lastY2    = 0.0;
 
     for(unsigned int i = 0; i < raysAzim; i++)
     {
-      std::cout << "a" << std::endl;
-
+      // TO DO: xWIDTH yWIDTH zHEIGHT factors as variables
       for(unsigned int j = 0; j < raysIncl; j++)
       {
-        std::cout << "b" << std::endl;
 
         idx = i * raysIncl + j;
 
         // fill pointcloud points
 
-        // AZIM: erster Quadrant: y konstant, x wächst, z kp
-        if(i <= 0.75 * raysAzimQuadrant)
+        // erster Quadrant A: y konstant, x wächst, z wächst mit j
+        if(i <= (0.75 * raysAzimQuadrant))
         {
-          cloud.points[idx].x = 0.1;
+          // cloud.points[idx].x = 0.1 + incrX;
+          cloud.points[idx].x = 0.1 + i * 0.015;
           cloud.points[idx].y = 2.0;
-          cloud.points[idx].z = 0.0;
-          std::cout << "cloud.points xyz = " << cloud.points[idx].x << " " << cloud.points[idx].y << " " << cloud.points[idx].z << std::endl;
-          counterAzim++;
-        }
+          cloud.points[idx].z = 1.0 + j * 0.1;
 
+          lastX = 0.1 + i * 0.015;
+        }
+        // erster Quadrant B, x konstant und y wird kleiner, z wächst mit j
+        // bis 25% zweiter Quadrant A same conditions
+        else if((i > (0.75 * raysAzimQuadrant)) && (i <= (1.25 * raysAzimQuadrant)))
+        {
+          unsigned int counter1B = i - 0.75 * raysAzimQuadrant;
+          cloud.points[idx].x    = lastX;
+          cloud.points[idx].y    = 2.0 - counter1B * 0.015;
+          cloud.points[idx].z    = 1.0 + j * 0.1;
+          // counter1B++;
+          lastY = 2.0 - counter1B * 0.015;
+        }
+        // zweiter Quadrant B: y konstant, x wird kleiner, z same
+        // bis 75% dritter Quadrant A same conditions
+        else if((i > (1.25 * raysAzimQuadrant)) && (i <= (2.75 * raysAzimQuadrant)))
+        // else if((i > (1.25 * raysAzimQuadrant)) && (i <= 900))
+        {
+          unsigned int counter2B = i - 1.25 * raysAzimQuadrant;
+          // std::cout << "counter2B = " << counter2B << std::endl;
+          cloud.points[idx].x = lastX - counter2B * 0.015;
+          cloud.points[idx].y = lastY;
+          cloud.points[idx].z = 1.0 + j * 0.1;
+          lastX2              = lastX - counter2B * 0.015;
+        }
+        // dritter Quadrant B: x konstant, y wird größer, z same
+        // bis 25% vierter Quadrant A same conditions
+        else if((i > (2.75 * raysAzimQuadrant)) && (i <= (3.25 * raysAzimQuadrant)))
+        {
+          unsigned int counter3B = i - 2.75 * raysAzimQuadrant;
+
+          cloud.points[idx].x = lastX2;
+          cloud.points[idx].y = lastY + counter3B * 0.015;
+          cloud.points[idx].z = 1.0 + j * 0.1;
+          lastY2              = lastY + counter3B * 0.015;
+        }
+        // vierter Quadrant B: y konstant, x wird größer, z same
         else
         {
-          std::cout << "else - counterAzim = " << counterAzim << std::endl;
-          cloud.points[idx].x = 3.0;
-          cloud.points[idx].y = 3.0;
-          cloud.points[idx].z = 3.0;
+          unsigned int counter4B = i - 3.25 * raysAzimQuadrant;
+
+          cloud.points[idx].x = lastX2 + counter4B * 0.015;
+          cloud.points[idx].y = lastY2;
+          cloud.points[idx].z = 1.0 + j * 0.1;
           // std::cout << "cloud.points xyz = " << cloud.points[idx].x << " " << cloud.points[idx].y << " " << cloud.points[idx].z << std::endl;
         }
       }
 
-      std::cout << "idx = " << idx << std::endl;
+      // std::cout << "counter2B: " << counter2B << std::endl;
+
+      // std::cout << "idx = " << idx << std::endl;
     }
 
     pubArtificialData.publish(cloud);
+
+    // ros::spin();
     ros::spinOnce();
     loop_rate.sleep();
   }
